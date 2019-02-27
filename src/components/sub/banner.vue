45bce47f9f44471b9572491a78329377879b7160
cunnan <cunnan@animals.com>
2019-02-28 06:58:58 +0800

    20190228

--- src/components/sub/banner.vue
+++ src/components/sub/banner.vue
@@ -1,6 +1,21 @@
 <template>
     <div class="j-banner">
-   
+        <div class="pics">
+            <a href=""><img src="../../assets/images/banner/banner1.jpg" alt=""></a>
+            <a href=""><img src="../../assets/images/banner/banner1.jpg" alt=""></a>
+            <a href=""><img src="../../assets/images/banner/banner1.jpg" alt=""></a>
+            <a href=""><img src="../../assets/images/banner/banner1.jpg" alt=""></a>
+            <a href=""><img src="../../assets/images/banner/banner1.jpg" alt=""></a>
+            <a href=""><img src="../../assets/images/banner/banner1.jpg" alt=""></a>
+        </div>
+        <div class="zsq">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
     </div>
 </template>
 <script>
@@ -9,7 +24,20 @@
     }
 </script>
 <style lang="scss" scoped>
+    // 轮播盒子
     .j-banner{
-       height:200px;
+       height: 40rem;
+       overflow: hidden;
    }
+    // 轮播条
+    .j-banner .pics{
+        width: 100%;
+        flex-grow: 0 0 auto;
+    }
+    .j-banner img{
+      
+    }
+    // 指示器
+    
+
 </style>
